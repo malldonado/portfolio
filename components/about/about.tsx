4bb434b3fc04eb8d4ff8ac5a3fc183baa2ac8082
malldonado <matheussilvacontat@gmail.com>
2024-07-03 11:33:53 -0300

    update project

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -17,17 +17,44 @@ function About({ language }: Props) {
   return (
     <div ref={ref} className="text-white pt-28 pb-8 about-container">
       {/* Aplica classe de animação slide-in condicionalmente com base em inView */}
-      <div className={`max-w-7xl mx-auto px-4 md:px-0 ${inView ? "slide-in visible" : "slide-in"}`}>
+      <div
+        className={`max-w-7xl mx-auto px-4 md:px-0 ${
+          inView ? "slide-in visible" : "slide-in"
+        }`}
+      >
         <div className="text-left">
-          <h1 className="text-4xl sm:text-5xl lg:text-7xl mb-10">{language === "en" ? "About Me" : "Sobre Mim"}</h1>
+          <h1 className="text-4xl sm:text-5xl lg:text-7xl mb-10">
+            {language === "en" ? "About Me" : "Sobre Mim"}
+          </h1>
           <p className="text-lg sm:text-2xl">
             {language === "en" ? (
               <>
-                Graduated in Systems Analysis and Development from FATEC and currently studying English at Influx, I worked as a front-end developer on the TCC animal adoption project. At Performa.ai, I worked on integrating virtual stores from various e-commerce platforms. I am always in constant study, learning Next.js, GraphQL, AWS, and Docker, in addition to contributing to projects available on my GitHub. My portfolio includes an app for the Art Burger hamburger restaurant, a project for the civil engineering company GTM Engenharia with a customizable panel, a blog, among others, and an application that locates stores and products near the user, called Fluxplaces.
+                Graduated in Systems Analysis and Development from FATEC and
+                currently studying English at Influx, I worked as a front-end
+                developer on the TCC project for animal adoption. At
+                Performa.ai, I worked as a Full Stack developer, integrating
+                virtual stores from various e-commerce platforms. I am
+                constantly studying, learning Next.js, GraphQL, AWS, and Docker,
+                and contributing to projects on my GitHub. My portfolio includes
+                an app for Art Burger hamburger shop, a customizable panel
+                project for civil engineering company GTM Engenharia, a blog,
+                among others, and an application called Fluxplaces that locates
+                nearby stores and products for users.
               </>
             ) : (
               <>
-                Formado em Análise e Desenvolvimento de Sistemas pela FATEC e atualmente cursando Inglês na Influx, atuei como desenvolvedor front-end no projeto TCC de adoção de animais. No Performa.ai, trabalhei realizando integrações de lojas virtuais de várias plataformas de ecommerce. Estou sempre em constante estudo, aprendendo Next.js, GraphQL, AWS e Docker, além de contribuir para projetos disponíveis no meu GitHub. Meu portfólio inclui um aplicativo para a hamburgueria Art Burger, um projeto para a empresa de engenharia civil GTM Engenharia com painel customizável, blog, entre outros, e uma aplicação que localiza lojas e produtos próximos ao usuário, chamada Fluxplaces.
+                Formado em Análise e Desenvolvimento de Sistemas pela FATEC e
+                atualmente cursando Inglês na Influx, atuei como desenvolvedor
+                front-end no projeto TCC de adoção de animais. No Performa.ai,
+                trabalhei como Full Stack realizando integrações de lojas
+                virtuais de várias plataformas de ecommerce. Estou sempre em
+                constante estudo, aprendendo Next.js, GraphQL, AWS e Docker,
+                além de contribuir para projetos disponíveis no meu GitHub. Meu
+                portfólio inclui um aplicativo para a hamburgueria Art Burger,
+                um projeto para a empresa de engenharia civil GTM Engenharia com
+                painel customizável, blog, entre outros, e uma aplicação que
+                localiza lojas e produtos próximos ao usuário, chamada
+                Fluxplaces.
               </>
             )}
           </p>
